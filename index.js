e3656156e715479cd041f7ec9f16e53c62f8a2d6
fiki574 <fiki8796@hotmail.com>
2023-10-06 15:21:41 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,45 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: "latest",
+    sourceType: "module",
+    project: "tsconfig.json",
+    tsconfigRootDir: __dirname,
+  },
+  plugins: ["react", "@typescript-eslint/eslint-plugin"],
+  ignorePatterns: [
+    ".github",
+    ".husky",
+    ".vscode",
+    "coverage",
+    "dist",
+    "node_modules",
+    "public",
+  ],
+  rules: {
+    indent: "off",
+    quotes: ["error", "double"],
+    semi: ["error", "always"],
+    "@typescript-eslint/interface-name-prefix": "off",
+    "@typescript-eslint/explicit-function-return-type": "off",
+    "@typescript-eslint/no-explicit-any": "off",
+    "@typescript-eslint/no-var-requires": 0,
+    "react/react-in-jsx-scope": "off",
+    "react/jsx-filename-extension": [
+      1,
+      { extensions: [".js", ".jsx", ".ts", ".tsx"] },
+    ],
+  },
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+};
